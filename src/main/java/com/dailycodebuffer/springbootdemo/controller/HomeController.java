13ff798235027a0823bf9604e2a2bfea73ac3d31
Aditya <adityasingh62065@gmail.com>
2023-07-15 08:26:30 +0530

    create RESTful API

--- src/main/java/com/dailycodebuffer/springbootdemo/controller/HomeController.java
+++ src/main/java/com/dailycodebuffer/springbootdemo/controller/HomeController.java
@@ -3,9 +3,9 @@ package com.dailycodebuffer.springbootdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
     @RequestMapping("/")
     public String home()
